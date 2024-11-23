32035e9cb762664d4cc014345830fd14c6d915aa
Pshemeeg <pshemeeg@gmail.com>
2024-11-23 14:02:57 +0100

    Zajecia1: Added project Zadanie2 - arithmetic mean

---
+++ Zajecia1/Zadanie2/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+ * Zadanie 1.2
+ * Napisz program, który oblicza średnią arytmetyczną dwóch liczb.
+ */
+
+namespace Zadanie2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
