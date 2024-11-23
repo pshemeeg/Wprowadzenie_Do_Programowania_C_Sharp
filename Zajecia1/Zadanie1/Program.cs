7de255c5ac4eb14092c8dd18dc0196cc88966173
Pshemeeg <pshemeeg@gmail.com>
2024-11-23 13:52:52 +0100

    Zajecia1: Initialization and Added solution Zadanie1 - greeting program

---
+++ Zajecia1/Zadanie1/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+ * Napisz program, który prosi o padanie twego imienia, a następnie wypisuje tekst w rodzaju:
+ *„Witaj Tomasz”. 
+ */
+namespace Zadanie1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string imie;
+            Console.WriteLine("Podaj swoje imię: ");
+            imie = Console.ReadLine();
+            Console.WriteLine("Witaj " + imie+"!");
+        }
+    }
+}
