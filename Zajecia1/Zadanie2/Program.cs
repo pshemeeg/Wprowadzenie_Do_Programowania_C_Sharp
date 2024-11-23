fa5b714082f5876582c583cec2aee6048c5477b2
Pshemeeg <pshemeeg@gmail.com>
2024-11-23 14:08:11 +0100

    Complete: Zadanie2 - arithmetic mean

--- Zajecia1/Zadanie2/Program.cs
+++ Zajecia1/Zadanie2/Program.cs
@@ -9,7 +9,12 @@ namespace Zadanie2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            double a, b;
+            Console.WriteLine("Podaj pierwszą liczbę");
+            a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj drugą liczbę");
+            b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Średnia arytmetyczna tych liczb wynosi: " + (a + b) / 2);
         }
     }
 }
