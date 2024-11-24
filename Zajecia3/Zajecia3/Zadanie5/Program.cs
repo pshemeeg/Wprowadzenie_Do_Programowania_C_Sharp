7026c9563af60956deb5bd3539840bd56bd8948d
Pshemeeg <pshemeeg@gmail.com>
2024-11-24 15:52:34 +0100

    Zadanie5: Complete

---
+++ Zajecia3/Zajecia3/Zadanie5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Zadanie5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] tab =
+            {
+                {3,8,0,2,6},
+                {1,3,5,7,3},
+                {1,2,3,4,5}
+            };
+            for (int w = 0; w < 3; w++)
+            {
+                for (int k = 0; k < 5; k++)
+                {
+                    Console.Write("{0} ", tab[w, k]);
+                }
+                Console.WriteLine();
+
+            }
+            Console.WriteLine("\nTranspozycja\n");
+            for (int w = 0; w < 5; w++)
+            {
+                for (int k = 0; k < 3; k++)
+                {
+                    Console.Write("{0} ", tab[k, w]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
