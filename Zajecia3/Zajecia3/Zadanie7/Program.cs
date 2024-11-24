68583f6a35c8262b5b80d37de27912c0222a95ee
Pshemeeg <pshemeeg@gmail.com>
2024-11-24 16:22:11 +0100

    Zadanie7: Complete try 1

---
+++ Zajecia3/Zajecia3/Zadanie7/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Zadanie7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string tekst;
+
+            Console.WriteLine("Wpisz zdanie: ");
+            tekst = Console.ReadLine();
+            for (int i = 0; i < tekst.Length; i++)
+            {
+                if ((tekst[i] == ' ') && (tekst[i + 1] == ' '))
+                    continue;
+                Console.Write(tekst[i]);
+            }
+        }
+    }
+}
