8604b34390a703ca0099b7b87d893490be8a401f
Pshemeeg <pshemeeg@gmail.com>
2024-11-24 14:53:11 +0100

    Zajecia3: Inicjalization Zadanie1: complete

---
+++ Zajecia3/Zajecia3/Zadanie1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Zadanie1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+            Console.WriteLine("Podaj długość tablicy: ");
+            n = int.Parse(Console.ReadLine());
+            int[] tab = new int[n];
+            for (int i = 0; i < tab.Length; i++)
+            {
+                Console.WriteLine("Podaj liczbę:");
+                tab[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Zawartość tablicy");
+            foreach (int number in tab)
+            {
+                Console.Write("{0}; ", number);
+            }
+        }
+    }
+}
