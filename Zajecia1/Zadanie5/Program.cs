4775ff5759e5c5057590873adb0fc94658886e5a
Pshemeeg <pshemeeg@gmail.com>
2024-11-25 12:56:30 +0100

    Zajecia1/Zadanie5: Created

---
+++ Zajecia1/Zadanie5/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+ * Zadanie5
+ * Napisz program, który oblicza trójmian kwadratowy.
+ */
+namespace Zadanie5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c, delta, x1, x2;
+            Console.WriteLine("Podaj a: ");
+            a = double.Parse(Console.ReadLine());
+        }
+    }
+}
