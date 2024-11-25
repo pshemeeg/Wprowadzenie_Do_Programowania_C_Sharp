cbbdaf6240e2db551213471881b27624664a911a
Pshemeeg <pshemeeg@gmail.com>
2024-11-25 12:46:32 +0100

    Zajecia1/Zadanie4: Created

---
+++ Zajecia1/Zadanie4/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+ * Zadanie 4
+ * Napisz program, który czyta trzy liczby (wszystkie różne) i wyświetla największą z nich. 
+ * W rozwiązaniu zastosuj instrukcje if z operatorem logicznym koniunkcji.
+ */
+namespace Zadanie4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
