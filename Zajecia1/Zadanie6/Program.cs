d8c4ca54f5cda9fea1a4f634d1e00f112a1c373d
Pshemeeg <pshemeeg@gmail.com>
2024-11-28 14:07:03 +0100

    Zadanie6: Better comments

--- Zajecia1/Zadanie6/Program.cs
+++ Zajecia1/Zadanie6/Program.cs
@@ -1,6 +1,6 @@
 ﻿/*
  * Zadanie6
- * Napisz program który oblicza wyrażenie:   {[(6,2:0,31-5/9*0,9)*0,2+0,15]:0,02}^3.
+ * Napisz program, który oblicza wyrażenie: {[(6,2:0,31-5/9*0,9)*0,2+0,15]:0,02}^3.
  */
 namespace Zadanie6;
 
