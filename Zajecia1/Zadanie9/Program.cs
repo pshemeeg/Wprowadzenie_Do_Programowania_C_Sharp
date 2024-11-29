d36b0c2e14e5ffba8c28215e3eeaf36c3ec10cf0
Pshemeeg <pshemeeg@gmail.com>
2024-11-29 12:35:02 +0100

    Zadanie9: Complete

--- Zajecia1/Zadanie9/Program.cs
+++ Zajecia1/Zadanie9/Program.cs
@@ -1,9 +1,28 @@
-﻿namespace Zadanie9;
+﻿/*
+ * Zadanie 9
+ * Podaj 3 różne liczby i wyświetl środkową (tzn. nie najmniejszą ani nie największą). 
+ * UWAGA! Jeżeli liczby nie będą różne, wypisz jakiś komunikat i zakończ program. 
+ */
+namespace Zadanie9;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        double a, b, c;
+        Console.Write("Podaj pierwszą liczbę: ");
+        a = double.Parse(Console.ReadLine() ?? string.Empty);
+        Console.Write("Podaj drugą liczbę: ");
+        b = double.Parse(Console.ReadLine() ?? string.Empty);
+        Console.Write("Podaj trzecią liczbę: ");
+        c = double.Parse(Console.ReadLine() ?? string.Empty);
+        if(a == b || b == c || c == a)
+            Console.WriteLine("Liczby muszą być różne!");
+        else if((a > b && a < c) || (a < b && a > c))
+            Console.WriteLine($"środkowa liczba to {a}.");
+        else if((b < a && b > c) || (b > a && b < c))
+            Console.WriteLine($"środkowa liczba to {b}.");
+        else
+            Console.WriteLine($"środkowa liczba to {c}.");
     }
 }
