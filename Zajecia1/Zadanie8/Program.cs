884ab13b54409d80434972bb53126c38c8cd7939
Pshemeeg <pshemeeg@gmail.com>
2024-11-29 12:22:52 +0100

    Zadanie 8: Done

--- Zajecia1/Zadanie8/Program.cs
+++ Zajecia1/Zadanie8/Program.cs
@@ -1,4 +1,5 @@
 ﻿/*
+ * Zadanie 8
  * Napisz program sprawdzający, czy podany rok jest rokiem przestępny, czy nim nie jest. 
  * UWAGA! Rok jest przestępny, jeśli spełnia jeden z następujących warunków: 
  * - jest podzielny przez 4, ale nie jest podzielny przez 100 
@@ -10,6 +11,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int rok;
+        Console.Write("Podaj rok: ");
+        rok = int.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
+        Console.WriteLine((rok%4==0 && rok%100!=0) || (rok%400==0) 
+            ?"ten rok jest przestępny!"
+            :"ten rok nie jest przestępny!"
+            );
     }
 }
