ec43f1a2441907ad5e43cee305dc5fd7558cc16c
Pshemeeg <pshemeeg@gmail.com>
2024-11-29 12:15:28 +0100

    Zadanie8: Created

---
+++ Zajecia1/Zadanie8/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+ * Napisz program sprawdzający, czy podany rok jest rokiem przestępny, czy nim nie jest. 
+ * UWAGA! Rok jest przestępny, jeśli spełnia jeden z następujących warunków: 
+ * - jest podzielny przez 4, ale nie jest podzielny przez 100 
+ * - jest podzielny przez 400
+ */
+namespace Zadanie8;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
