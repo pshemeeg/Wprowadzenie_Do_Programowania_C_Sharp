2c809b09e319833a8a7e53ba749a8ad04225a3ec
Pshemeeg <pshemeeg@gmail.com>
2024-11-29 12:13:15 +0100

    Zadanie7: Done

--- Zajecia1/Zadanie7/Program.cs
+++ Zajecia1/Zadanie7/Program.cs
@@ -1,9 +1,20 @@
-﻿namespace Zadanie7;
+﻿/*
+ * Zadanie 7
+ * Napisz program sprawdzający, która liczba jest większa: pierwiastek z dwóch do potęgi 
+ * pierwiastek z trzech czy pierwiastek z trzech do potęgi pierwiastek z dwóch. Uwaga! W 
+ * rozwiązaniu użyj metod Pow() i Sqrt().
+ */
+namespace Zadanie7;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        double a, b;
+        a = double.Pow(double.Sqrt(2), double.Sqrt(3));
+        b = double.Pow(double.Sqrt(3), double.Sqrt(2));
+        Console.WriteLine(a > b
+            ? "pierwiastek z dwóch do potęgi pierwiastek z trzech"
+            : "pierwiastek z trzech do potęgi pierwiastek z dwóch");
     }
 }
